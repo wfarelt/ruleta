348855087ffd2e8016b2bfd95eb0e79125888ac4
wiwi farel <wfarel.t@gmail.com>
2026-02-24 23:13:45 -0400

    Versión PRO

--- draws/views.py
+++ draws/views.py
@@ -1,8 +1,11 @@
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, redirect
 from django.contrib import messages
 from pasanaku.models import Pasanaku, Participation
 from .services import perform_draw
+
 
 def execute_draw(request, pasanaku_id):
 
@@ -10,18 +13,18 @@ def execute_draw(request, pasanaku_id):
 
     winner = perform_draw(pasanaku)
 
-    if winner:
-        messages.success(
-            request,
-            f"Winner: {winner.participant.full_name}"
-        )
-    else:
-        messages.warning(
-            request,
-            "No more participants available for draw."
-        )
+    if not winner:
+        return JsonResponse({
+            "success": False,
+            "message": "No participants available."
+        })
 
-    return redirect('admin:pasanaku_pasanaku_changelist')
+    return JsonResponse({
+        "success": True,
+        "winner_id": winner.id,
+        "winner_name": winner.participant.full_name,
+        "position": winner.winning_position
+    })
 
 
 def roulette_view(request, pasanaku_id):
